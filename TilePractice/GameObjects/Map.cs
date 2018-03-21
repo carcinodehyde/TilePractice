2f8f99d31df2cb3657035111d1368b6eecfd90ce
Deddy S <deddy.skyblue@gmail.com>
2018-03-21 17:09:34 +0700

    init

---
+++ TilePractice/GameObjects/Map.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TilePractice.GameObjects
+{
+    class Map
+    {
+        Texture2D Tiles;
+
+        int[,] TileMap;
+
+        int TileWidth = 32;
+        int TileHeight = 32;
+
+        private ContentManager Content;
+
+        public Map(String name, ContentManager content)
+        {
+            Content = content;
+
+            LoadMapData(name);
+            LoadTileTextures();
+        }
+
+        private void LoadTileTextures()
+        {
+
+            Tiles = Content.Load<Texture2D>("Graphics/Tile1");
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            for (int y = 0; y < TileMap.GetLength(1); y++)
+            {
+
+                for (int x = 0; x < TileMap.GetLength(0); x++)
+                {
+
+                    spriteBatch.Draw(Tiles,
+                        new Vector2(x * TileWidth, y * TileHeight),
+                        new Rectangle((TileMap[x, y] % 8) * 32, (TileMap[x, y] / 8) * 32, 32, 32),
+                        Color.White);
+                }
+            }
+        }
+
+        private void LoadMapData(String name)
+        {
+
+            String path = Path.Combine(Environment.CurrentDirectory + "\\MapLayouts\\" + name + ".txt");
+
+            int width = 0;
+            int height = File.ReadLines(path).Count();
+
+            StreamReader sr = new StreamReader(path);
+
+            string line = sr.ReadLine();
+            string[] tileNo = line.Split(',');
+
+            width = tileNo.Count();
+
+            TileMap = new int[height, width];
+
+            sr = new StreamReader(path);
+
+            for (int y = 0; y < height; y++)
+            {
+
+                line = sr.ReadLine();
+                tileNo = line.Split(',');
+
+                for (int x = 0; x < width; x++)
+                {
+
+                    TileMap[y, x] = Convert.ToInt32(tileNo[x]);
+                }
+            }
+
+            sr.Close();
+
+        }
+    }
+}
---
+++ TilePractice/Main.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using TilePractice.GameObjects;
+
+namespace TilePractice
+{
+    public class Main : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Map map;
+
+        public Main()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            var halfWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2;
+            var halfHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / 2;
+
+            this.Window.Position = new Point(halfWidth - (halfWidth/2), halfHeight - (halfHeight/2));
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            map = new Map("Map1", Content);
+        }
+
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            map.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
