0d962af897a287373beff0e1c21826aa82925220
Deddy S <deddy.skyblue@gmail.com>
2018-03-22 10:04:25 +0700

    add Sprite object

---
+++ TilePractice/GameObjects/Sprite.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TilePractice.GameObjects
+{
+    class Sprite
+    {
+        private Vector2 Position { get; set; }
+        private Texture2D Texture { get; set; }
+
+        public Sprite(Texture2D texture, Vector2 position)
+        {
+
+            Texture = texture;
+            Position = position;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            spriteBatch.Draw(Texture, Position, Color.White);
+        }
+    }
+}
